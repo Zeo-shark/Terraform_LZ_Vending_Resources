d0ae96bef7fe5f8a59c4d5da95a8faaefa00c9c4
Zeo-shark <sauravberaiit@gmail.com>
2023-08-28 11:44:12 +0530

    removed unnecesary lines

--- policies.tf
+++
@@ -1,7 +0,0 @@
-variable "default_budget_config" {
-  description = "Default budget configuration"
-  type        = map(map(string))
-  # Define default budget configurations here
-}
-
-# Define default budgets using the variable
