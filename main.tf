1fcb2c76c255c21c16515c8643a16c85a89f1ea7
Zeo-shark <sauravberaiit@gmail.com>
2023-08-28 14:26:27 +0530

    updated policy definitions

--- main.tf
+++ main.tf
@@ -52,26 +52,32 @@ resource "azurerm_policy_definition" "example" {
 }
 
 
-resource "azurerm_policy_assignment" "example" {
-  name                 = "example-policy-assignment"
-  scope                = azurerm_resource_group.example.id
-  policy_definition_id = azurerm_policy_definition.example.id
-  description          = "Policy Assignment created via an Acceptance Test"
-  display_name         = "My Example Policy Assignment"
-
-  metadata = <<METADATA
-    {
-    "category": "General"
-    }
-METADATA
+resource "azurerm_policy_set_definition" "example" {
+  name         = "testPolicySet"
+  policy_type  = "Custom"
+  display_name = "Test Policy Set"
 
   parameters = <<PARAMETERS
-{
-  "allowedLocations": {
-    "value": [ "West Europe" ]
-  }
-}
+    {
+        "allowedLocations": {
+            "type": "Array",
+            "metadata": {
+                "description": "The list of allowed locations for resources.",
+                "displayName": "Allowed locations",
+                "strongType": "location"
+            }
+        }
+    }
 PARAMETERS
+
+  policy_definition_reference {
+    policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/e765b5de-1225-4ba3-bd56-1ac6695af988"
+    parameter_values     = <<VALUE
+    {
+      "listOfAllowedLocations": {"value": "[parameters('allowedLocations')]"}
+    }
+    VALUE
+  }
 }
 
 # budgets plans
