0c56d0f2bae890ac43df9d2021c583d86ad5f400
Zeo-shark <sauravberaiit@gmail.com>
2023-08-28 14:44:25 +0530

    added the updated code

--- main.tf
+++ main.tf
@@ -81,27 +81,84 @@ PARAMETERS
 }
 
 # budgets plans
-resource "azurerm_budget" "example" {
-  name                  = "example-budget"
-  resource_group_name   = azurerm_resource_group.example.name
-  amount                = 100.0
+resource "azurerm_monitor_action_group" "example" {
+  name                = "example"
+  resource_group_name = azurerm_resource_group.example.name
+  short_name          = "example"
+}
+
+resource "azurerm_consumption_budget_subscription" "example" {
+  name            = "example"
+  subscription_id = data.azurerm_subscription.current.id
+
+  amount     = 1000
+  time_grain = "Monthly"
+
   time_period {
-    start_date = "2023-01-01"
-    end_date   = "2023-12-31"
+    start_date = "2022-06-01T00:00:00Z"
+    end_date   = "2022-07-01T00:00:00Z"
+  }
+
+  filter {
+    dimension {
+      name = "ResourceGroupName"
+      values = [
+        azurerm_resource_group.example.name,
+      ]
+    }
+
+    tag {
+      name = "foo"
+      values = [
+        "bar",
+        "baz",
+      ]
+    }
+  }
+
+  notification {
+    enabled   = true
+    threshold = 90.0
+    operator  = "EqualTo"
+
+    contact_emails = [
+      "foo@example.com",
+      "bar@example.com",
+    ]
+
+    contact_groups = [
+      azurerm_monitor_action_group.example.id,
+    ]
+
+    contact_roles = [
+      "Owner",
+    ]
+  }
+
+  notification {
+    enabled        = false
+    threshold      = 100.0
+    operator       = "GreaterThan"
+    threshold_type = "Forecasted"
+
+    contact_emails = [
+      "foo@example.com",
+      "bar@example.com",
+    ]
   }
 }
 
-#budget alert
-resource "azurerm_budget_alert" "rg_alert" {
-  name               = "rg-budget-alert"
-  resource_group_name = azurerm_resource_group.example.name
-  budget_id          = azurerm_budget.rg_budget.id
+# #budget alert
+# resource "azurerm_budget_alert" "rg_alert" {
+#   name               = "rg-budget-alert"
+#   resource_group_name = azurerm_resource_group.example.name
+#   budget_id          = azurerm_budget.rg_budget.id
 
-  threshold = 70
-  direction = "Up"
-  operator  = "GreaterThan"
-  time_aggregation = "Average"
-}
+#   threshold = 70
+#   direction = "Up"
+#   operator  = "GreaterThan"
+#   time_aggregation = "Average"
+# }
 
 #rabac roles subscription and resource groups level
 resource "azurerm_role_assignment" "rbac_owner" {
@@ -231,10 +288,10 @@ resource "azurerm_virtual_machine" "example" {
   }
 }
 
-resource "azurerm_ddos_protection_plan_association" "example" {
-  resource_uri       = azurerm_virtual_machine.example.id
-  protection_plan_id = azurerm_ddos_protection_plan.example.id
-}
+# resource "azurerm_ddos_protection_plan_association" "example" {
+#   resource_uri       = azurerm_virtual_machine.example.id
+#   protection_plan_id = azurerm_ddos_protection_plan.example.id
+# }
 
 #  siem integration, defender for cloud 
 resource "azurerm_log_analytics_workspace" "example" {
