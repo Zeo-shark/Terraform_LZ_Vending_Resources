2afb889352345f5b21e4615a4fcc54c5b00ec1a9
Zeo-shark <sauravberaiit@gmail.com>
2023-08-28 11:39:28 +0530

    minor changes

--- tags.tf
+++ tags.tf
@@ -1,13 +1,13 @@
-variable "tags_subscriptions" {
-  description = "Tags for subscriptions"
-  type        = map(map(list(string)))
-  # Define tags for subscriptions here
-}
+# variable "tags_subscriptions" {
+#   description = "Tags for subscriptions"
+#   type        = map(map(list(string)))
+#   # Define tags for subscriptions here
+# }
 
-variable "tags_resource_groups" {
-  description = "Tags for resource groups"
-  type        = map(map(list(string)))
-  # Define tags for resource groups here
-}
+# variable "tags_resource_groups" {
+#   description = "Tags for resource groups"
+#   type        = map(map(list(string)))
+#   # Define tags for resource groups here
+# }
 
-# Assign tags to subscriptions and resource groups using the variables
+# # Assign tags to subscriptions and resource groups using the variables
